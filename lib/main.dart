0880719d2c37f79ecbfa2c7b5cc5bcff114fbf95
MacPom <152692308+MacPom@users.noreply.github.com>
2024-01-27 11:41:22 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,221 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(CalculatorApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CalculatorScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CalculatorScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CalculatorScreenState createState() => _CalculatorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String displayText = "0";
 
-  void _incrementCounter() {
+  static const addSign = "\u002B";
+  static const subtractSign = "\u2212";
+  static const multiplySign = "\u00D7";
+  static const divideSign = "\u00F7";
+  static const equalSign = "\u003D";
+
+  void updateDisplay(String value) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (displayText == "0" || displayText == "Error") {
+        displayText = value;
+      } else {
+        displayText += value;
+      }
+    });
+  }
+
+  void clearDisplay() {
+    setState(() {
+      displayText = "0";
+    });
+  }
+
+  void backspace() {
+    setState(() {
+      if (displayText.length > 1) {
+        displayText = displayText.substring(0, displayText.length - 1);
+      } else {
+        displayText = "0";
+      }
+    });
+  }
+
+  void calculateResult() {
+    setState(() {
+      displayText = "0";
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: null,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  displayText,
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          buildRow([clearButton(), backspaceButton()]),
+          buildRow([
+            numberButton("7"),
+            numberButton("8"),
+            numberButton("9"),
+            operatorButton(divideSign)
+          ]),
+          buildRow([
+            numberButton("4"),
+            numberButton("5"),
+            numberButton("6"),
+            operatorButton(multiplySign)
+          ]),
+          buildRow([
+            numberButton("1"),
+            numberButton("2"),
+            numberButton("3"),
+            operatorButton(subtractSign)
+          ]),
+          buildRow([numberButton("0"), operatorButton(addSign)]),
+          buildRow([operatorButton(equalSign)]),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+
+  Widget buildRow(List<Widget> buttons) {
+    return Expanded(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: buttons,
+      ),
+    );
+  }
+
+  Widget numberButton(String value) {
+    return value == "0"
+        ? Flexible(
+            flex: 3,
+            child: FractionallySizedBox(
+              widthFactor: 1.0,
+              child: GestureDetector(
+                onTap: () => updateDisplay(value),
+                child: Container(
+                  color: Colors.greenAccent,
+                  child: Center(
+                    child: Text(
+                      value,
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          )
+        : Flexible(
+            child: FractionallySizedBox(
+              widthFactor: 1.0,
+              child: GestureDetector(
+                onTap: () => updateDisplay(value),
+                child: Container(
+                  color: Colors.greenAccent,
+                  child: Center(
+                    child: Text(
+                      value,
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ],
+          );
+  }
+
+  Widget operatorButton(String value) {
+    return Flexible(
+      child: FractionallySizedBox(
+        widthFactor: 1.0,
+        child: GestureDetector(
+          onTap: () {
+            if (value == equalSign) {
+              calculateResult();
+            } else {
+              updateDisplay(value);
+            }
+          },
+          child: Container(
+            color: value == equalSign ? Colors.green[700] : Colors.grey,
+            child: Center(
+              child: Text(
+                value,
+                style: TextStyle(fontSize: 25.0),
+              ),
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget clearButton() {
+    return Flexible(
+      child: FractionallySizedBox(
+        widthFactor: 1.0,
+        child: GestureDetector(
+          onTap: clearDisplay,
+          child: Container(
+            color: Colors.grey,
+            child: Center(
+              child: Text(
+                "C",
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget backspaceButton() {
+    return Flexible(
+      child: FractionallySizedBox(
+        widthFactor: 1.0,
+        child: GestureDetector(
+          onTap: () => backspace(),
+          child: Container(
+            color: Colors.grey,
+            child: Center(
+              child: Icon(Icons.backspace),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
